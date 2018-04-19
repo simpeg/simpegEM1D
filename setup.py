bcdd66334e9e620490435924fcc9eeb5e87733ad
seogi_macbook <sgkang09@github.com>
2018-04-18 20:51:47 -0700

    working on deploying pipit

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
-import os
-try:
-    import SimPEG
-except ImportError, e:
-    os.system('git clone https://github.com/simpeg/simpeg.git')
-    os.system('mv simpeg/SimPEG temp')
-    os.system('rm -rf simpeg')
-    os.system('mv temp SimPEG')
+from distutils.core import setup
+setup(
+  name='simpegEM1D',
+  packages=['simpegEM1D'],
+  version='0.1',
+  description='Simulationa and inversion electromagnetic geophysics',
+  author='Seogi Kang',
+  author_email='sgkang09@gmail.com',
+  url='https://github.com/simpeg/simpegEM1D',
+  download_url='https://github.com/simpeg/simpegEM1D/archive/0.1.tar.gz',
+  keywords=['electromagnetic', 'geophysics', 'airborne'],
+  classifiers=[],
+)
+
