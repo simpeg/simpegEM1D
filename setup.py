57a7724e98ab0607a53ccda0a3b55bf48fc0dfc5
seogi_macbook <sgkang09@github.com>
2018-04-21 23:12:44 -0700

    update

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open('README.md') as f:
 
 setup(
     name = 'simpegEM1D',
-    version = '0.0.1',
+    version = '0.0.2',
     packages = find_packages(),
     install_requires = [
         'SimPEG>=0.4.1',
