c10a7b8e59127526dddc03da66b4cec8eaef31d0
seogi_macbook <sgkang09@github.com>
2018-05-18 16:01:20 -0700

    use latest empymod

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     packages = find_packages(),
     install_requires = [
         'SimPEG>=0.4.1',
-        'empymod',
+        'empymod>=1.6.2',
         'multiprocess'
     ],
     author = 'Seogi Kang',
