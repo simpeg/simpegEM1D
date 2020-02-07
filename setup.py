41420abab2b710e98866fb738f72c2295b14ec97
seogi_macbook <sgkang09@gmail.com>
2020-02-07 12:00:48 -0800

    0.0.18

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ fExt = [Extension(name='simpegEM1D.m_rTE_Fortran', # Name of the package to impo
 
 setup(
     name='simpegEM1D',
-    version='0.0.18a',
+    version='0.0.18',
     packages=find_packages(),
     install_requires=[
         'SimPEG>=0.4.1',
