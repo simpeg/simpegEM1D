f5d262eca1bd61e170cdc54360f9b743f479cdb7
seogi_macbook <sgkang09@gmail.com>
2020-10-02 17:55:02 -0700

    add test

--- tests/testEM1D_TD_dual_moment_fwd.py
+++ tests/testEM1D_TD_dual_moment_fwd.py
@@ -1,12 +1,9 @@
 import unittest
-from SimPEG import Mesh, Maps, Utils
 from SimPEG import maps
-from SimPEG.utils import mkvc
 import matplotlib.pyplot as plt
 import simpegEM1D as em1d
-from simpegEM1D.known_waveforms import piecewise_ramp
+from simpegEM1D.known_waveforms import piecewise_ramp, piecewise_pulse
 from simpegEM1D.analytics import *
-from simpegEM1D.waveforms import TriangleFun
 from simpegEM1D import skytem_HM_2015, skytem_LM_2015
 import numpy as np
 from scipy import io
@@ -22,87 +19,103 @@ class EM1D_TD_FwdProblemTests(unittest.TestCase):
         time_HM = wave_HM.time_gate_center[0::2]
         time_LM = wave_LM.time_gate_center[0::2]
 
-        thicknesses = np.ones(5) * 10.
+
+        source_location = np.array([0., 0., 0.])
+        source_orientation = "z"  # "x", "y" or "z"
+        source_current = 1.
+        source_radius = 10.
+        moment_amplitude=1.
+
+        receiver_location = np.array([10., 0., 0.])
+        receiver_orientation = "z"  # "x", "y" or "z"
+        field_type = "secondary"  # "secondary", "total" or "ppm"
+
+        times = np.logspace(-5, -2, 41)
+
+        # Receiver list
+        rx = em1d.receivers.TimeDomainPointReceiver(
+                receiver_location,
+                times=time_HM,
+                times_dual_moment=time_LM,
+                orientation=receiver_orientation,
+                component="dbdt"
+        )
+        receiver_list = [rx]
+
+        # Sources
 
         time_input_currents_HM = wave_HM.current_times[-7:]
         input_currents_HM = wave_HM.currents[-7:]
         time_input_currents_LM = wave_LM.current_times[-13:]
         input_currents_LM = wave_LM.currents[-13:]
 
-        TDsurvey = EM1DSurveyTD(
-            rx_location=np.array([0., 0., 100.]),
-            src_location=np.array([0., 0., 100.]),
-            topo=np.r_[0., 0., 100.],
-            depth=depth,
-            rx_type='dBzdt',
-            wave_type='general',
-            src_type='CircularLoop',
-            a=13.,
-            I=1.,
-            time=time_HM,
+
+        src = em1d.sources.TimeDomainHorizontalLoopSource(
+            receiver_list=receiver_list,
+            location=source_location,
+            I=source_current,
+            a=source_radius,
+            wave_type="general",
+            moment_type='dual',
             time_input_currents=time_input_currents_HM,
             input_currents=input_currents_HM,
-            n_pulse=2,
-            base_frequency=25.,
-            use_lowpass_filter=False,
-            high_cut_frequency=7e4,
-            moment_type='dual',
-            time_dual_moment=time_HM,
-            time_input_currents_dual_moment=time_input_currents_LM,
-            input_currents_dual_moment=input_currents_LM,
-            base_frequency_dual_moment=210,
+            n_pulse = 1,
+            base_frequency = 25.,
+            time_input_currents_dual_moment = time_input_currents_LM,
+            input_currents_dual_moment = input_currents_LM,
+            base_frequency_dual_moment = 210
+        )
+        source_list = [src]
+
+        thicknesses = np.ones(3)
+        sigma = 1e-2
+        n_layer = thicknesses.size + 1
+
+        sigma_model = sigma * np.ones(n_layer)
+        survey = em1d.survey.EM1DSurveyTD(source_list)
+        model_mapping = maps.IdentityMap(nP=n_layer)
+        simulation = em1d.simulation.EM1DTMSimulation(
+            survey=survey, thicknesses=thicknesses, sigmaMap=model_mapping,
         )
 
-        sig_half=1e-2
-        chi_half=0.
 
-        expmap = Maps.ExpMap(mesh1D)
-        m_1D = np.log(np.ones(TDsurvey.n_layer)*sig_half)
-        chi = np.zeros(TDsurvey.n_layer)
-
-        prob = EM1D(
-            mesh1D, sigmaMap=expmap, chi=chi
-        )
-        prob.pair(TDsurvey)
-
-        self.survey = TDsurvey
-        self.prob = prob
-        self.mesh1D = mesh1D
+        self.survey = survey
+        self.simulation = simulation
         self.showIt = False
-        self.chi = chi
-        self.m_1D = m_1D
-        self.sig_half = sig_half
-        self.expmap = expmap
+        self.sigma_model = sigma_model
+        self.sigma_halfspace = sigma
 
     def test_em1dtd_circular_loop_single_pulse(self):
 
-        dBzdtTD = self.survey.dpred(self.m_1D)
-        dBzdtTD_HM = dBzdtTD[:self.survey.n_time]
-        dBzdtTD_LM = dBzdtTD[self.survey.n_time:]
+        src = self.survey.srcList[0]
+        rx = src.rxList[0]
+        dBzdtTD = self.simulation.dpred(self.sigma_model)
+        dBzdtTD_HM = dBzdtTD[:rx.times.size]
+        dBzdtTD_LM = dBzdtTD[rx.times.size:]
 
         def step_func_dBzdt(time):
-            return EM1DAnalytics.dBzdtAnalCircT(
-                self.survey.a, time, self.sig_half
+            return dBzdt_horizontal_circular_loop(
+                src.a, time, self.sigma_halfspace
             )
 
         dBzdtTD_analytic_HM = piecewise_pulse(
-            step_func_dBzdt, self.survey.time,
-            self.survey.time_input_currents,
-            self.survey.input_currents,
-            self.survey.period
+            step_func_dBzdt, rx.times,
+            src.time_input_currents,
+            src.input_currents,
+            src.period
         )
 
         dBzdtTD_analytic_LM = piecewise_pulse(
-            step_func_dBzdt, self.survey.time,
-            self.survey.time_input_currents_dual_moment,
-            self.survey.input_currents_dual_moment,
-            self.survey.period_dual_moment
+            step_func_dBzdt, rx.times_dual_moment,
+            src.time_input_currents_dual_moment,
+            src.input_currents_dual_moment,
+            src.period_dual_moment
         )
         if self.showIt:
-            plt.loglog(self.survey.time, -dBzdtTD_HM)
-            plt.loglog(self.survey.time, -dBzdtTD_LM)
-            plt.loglog(self.survey.time, -dBzdtTD_analytic_HM, 'x')
-            plt.loglog(self.survey.time, -dBzdtTD_analytic_LM, 'x')
+            plt.loglog(rx.times, -dBzdtTD_HM)
+            plt.loglog(rx.times_dual_moment, -dBzdtTD_LM)
+            plt.loglog(rx.times, -dBzdtTD_analytic_HM, 'x')
+            plt.loglog(rx.times_dual_moment, -dBzdtTD_analytic_LM, 'x')
             plt.show()
 
         err = (
